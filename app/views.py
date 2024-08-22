9adb32c60daa1f0cddd540472810e959453f2238
pedrelias <pedreliasz224@gmail.com>
2024-08-21 22:11:26 -0300

    commit

--- app/views.py
+++ app/views.py
@@ -26,13 +26,43 @@ def f1_view(request):
     f1_categoria = CategoriaAutomobilismo.objects.get(nome='Formula 1')
     corridas_f1 = Corrida.objects.filter(categoria=f1_categoria)
     calendario_f1 = Calendario.objects.filter(corrida__in=corridas_f1)
-    equipes_f1 = Equipe.objects.filter(corrida__in=corridas_f1)
-    pilotos_f1 = Piloto.objects.filter(equipe__in=equipes_f1)
-    
+    equipes = Equipe.objects.all()
+    pilotos = Piloto.objects.all()
+
     context = {
         'calendario': calendario_f1,
-        'equipes': equipes_f1,
-        'pilotos': pilotos_f1,
+        'equipes': equipes,
+        'pilotos': pilotos,
     }
-    
-    return render(request, 'f1.html', context)
+
+    return render(request, 'f1/f1.html', context)
+
+def nascar_view(request):
+    nascar_categoria = CategoriaAutomobilismo.objects.get(nome='NASCAR')
+    corridas_nascar = Corrida.objects.filter(categoria=nascar_categoria)
+    calendario_nascar = Calendario.objects.filter(corrida__in=corridas_nascar)
+    equipes = Equipe.objects.all()
+    pilotos = Piloto.objects.all()
+
+    context = {
+        'calendario': calendario_nascar,
+        'equipes': equipes,
+        'pilotos': pilotos,
+    }
+
+    return render(request, 'nascar/nascar.html', context)
+
+def indy_view(request):
+    indy_categoria = CategoriaAutomobilismo.objects.get(nome='IndyCar')
+    corridas_indy = Corrida.objects.filter(categoria=indy_categoria)
+    calendario_indy = Calendario.objects.filter(corrida__in=corridas_indy)
+    equipes = Equipe.objects.all()
+    pilotos = Piloto.objects.all()
+
+    context = {
+        'calendario': calendario_indy,
+        'equipes': equipes,
+        'pilotos': pilotos,
+    }
+
+    return render(request, 'indy/indy.html', context)
--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,15 @@
 
 from django.contrib import admin
 from app import views
+from app.views import *
 from django.views.generic import TemplateView
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
-    path('f1/', TemplateView.as_view(template_name='f1/f1.html'), name='f1'),
-    path('nascar/', TemplateView.as_view(template_name='nascar/nascar.html'), name='nascar'),
-    path('indy/', TemplateView.as_view(template_name='indy/indy.html'), name='indy'),
+    path('f1/', f1_view, name='f1'),
+    path('indy/', indy_view, name='indy'),
+    path('nascar/', nascar_view, name='nascar'),
 ]
 
